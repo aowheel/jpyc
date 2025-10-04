9f9f389eea614796eb1bb7692a85edf713597d86
aowheel <aoimakino2003@gmail.com>
2025-10-05 02:24:15 +0900

    add viem client

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-import { jpyc } from "./utils/client";
+import { JPYC_V2_ABI } from "@jpyc/sdk-v1";
+import { JPYC_ADDRESS } from "./config/constants";
+import { jpyc } from "./utils/jpyc-client";
+import { publicClient } from "./utils/viem-client";
 
 jpyc.totalSupply().then((totalSupply) => {
 	console.log(`Total Supply: ${totalSupply.toString()}`);
 });
+
+publicClient
+	.readContract({
+		address: JPYC_ADDRESS,
+		abi: JPYC_V2_ABI,
+		functionName: "name",
+	})
+	.then((name) => {
+		console.log(`Token Name: ${name}`);
+	});
